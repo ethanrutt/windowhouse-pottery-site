4f3df67ef23de98b1b32789959c9ad0ebef32ed2
Ethan Rutt <122636652+ethanrutt@users.noreply.github.com>
2024-07-27 17:27:04 -0500

    Navbar style (#1)
    * cleanup of files
    
    * gallery page

---
+++ src/pages/GalleryElements.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  justify-content: center;
+  width: 90%;
+  margin: auto;
+`;
+
+export const Tr = styled.tr`
+  width: 100%;
+`
+
+export const Td = styled.td`
+  height: 100%;
+  width: 33%;
+  padding: 10px;
+`
+
+export const Image = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+`
+
