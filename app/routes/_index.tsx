19ffc82419845329a4edd9f8776f798df3e3326c
ethanrutt <ethan.rutt2032@gmail.com>
2025-07-11 15:26:48 -0500

    home page image styling WIP

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,14 @@
 import logo from "./../images/windowhouse_logo.png";
+import house from "./../images/kim_house.jpg";
 
 export default function Home() {
     return (
         <>
             <h1>Hi from Home</h1>
-            <img src={logo} />
+            <div className="">
+                <img className="home_img_container__img" src={logo} />
+                <img className="home_img_container__img" src={house} />
+            </div>
         </>
     );
 }
